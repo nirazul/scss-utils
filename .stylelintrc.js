ab6c0f9676e704c65093e5fb2b900838cd96f7b1
nirazul <rouven@adorable-squid.ch>
2024-01-30 15:00:50 +0100

    Fix stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-recommended-scss',
-    'stylelint-config-prettier',
   ],
   'rules': {
     indentation: [2, { severity: 'error' }],
